ff7d411446f7e114640259168851f45d65f76043
C.K. Chan <chanchikin@ied.edu.hk>
2016-10-15 14:53:48 +0800

    add register activity

--- app/src/main/java/com/example/ck/simpleblog/MainActivity.java
+++ app/src/main/java/com/example/ck/simpleblog/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ck.simpleblog;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,24 +10,49 @@ import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-//https://www.youtube.com/watch?v=Yx8lRrclCYo&index=20&list=PLGCjwl1RrtcTXrWuRTa59RyRmQ4OedWrt#t=14.086621
+import com.squareup.picasso.Picasso;
+//https://www.youtube.com/watch?v=jmOEeJ4CFH4&index=24&list=PLGCjwl1RrtcTXrWuRTa59RyRmQ4OedWrt
 //14:28
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mBlogList;
     private DatabaseReference mDatabase;
+    //Simple Blog App - Register / Sign Up - Part 7
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Simple Blog App - Register / Sign Up - Part 7
+        //Check the user is login or not
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null){
+                        Intent loginintent = new Intent(MainActivity.this, RegisterActivity.class);
+                        loginintent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(loginintent);
+
+                }
+            }
+        };
+
+
+
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Blog");
 
         mBlogList = (RecyclerView) findViewById(R.id.blog_list);
@@ -37,6 +64,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        //start of Simple Blog App - Register / Sign Up - Part 7
+
+        mAuth.addAuthStateListener(mAuthListener);
+
+        //end of Simple Blog App - Register / Sign Up - Part 7
 
         FirebaseRecyclerAdapter<Blog, BlogViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Blog, BlogViewHolder>(
                 Blog.class,
@@ -49,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
             protected void populateViewHolder(BlogViewHolder viewHolder, Blog model, int position) {
                     viewHolder.setTitle(model.getTitle());
                 viewHolder.setDesc(model.getDesc());
+                viewHolder.setImage(getApplicationContext(), model.getImage());
             }
         };
 
@@ -75,6 +108,11 @@ public class MainActivity extends AppCompatActivity {
             TextView post_dsec = (TextView)mView.findViewById(R.id.post_desc);
             post_dsec.setText(desc);
 
+        }
+
+        public void setImage(Context ctx, String image){
+            ImageView post_image = (ImageView) mView.findViewById(R.id.post_image);
+            Picasso.with(ctx).load(image).into(post_image);
         }
     }
 
